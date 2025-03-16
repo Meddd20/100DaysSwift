9015a054b36f185d23c32de768cc1ac313f86141
Medhiko Biraja <mbirajabiraja@gmail.com>
2025-03-16 20:03:50 +0800

    Initial Commit

---
+++ Notes/NotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesApp.swift
+//  Notes
+//
+//  Created by Medhiko Biraja on 16/03/25.
+//
+
+import SwiftUI
+
+@main
+struct NotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NotesTests/NotesTests.swift
@@ -0,0 +1,17 @@
+//
+//  NotesTests.swift
+//  NotesTests
+//
+//  Created by Medhiko Biraja on 16/03/25.
+//
+
+import Testing
+@testable import Notes
+
+struct NotesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
