7f76e09a2e89207630e62b324a8ca1817659d423
Medhiko Biraja <mbirajabiraja@gmail.com>
2025-03-24 15:45:08 +0800

    feat(GuestFlag): Implement flag selection, answer validation, and UI structure.

--- Notes/GuestFlag.swift
+++ Notes/GuestFlag.swift
@@ -6,21 +6,76 @@
 //
 
 import SwiftUI
-
 struct GuestFlag: View {
+    @State private var isAlertShown = false
+    @State private var countries = ["Estonia", "France", "Germany", "Ireland","Italy", "Monaco", "Nigeria", "Poland", "Spain", "UK", "Ukraine", "US"].shuffled()
+    @State private var correctAnswer = Int.random(in: 0..<3)
+    @State private var score = 0
+    @State private var scoreTitle = ""
+    @State private var showScore = false
+    
     var body: some View {
-        
-        
-        ZStack (){
-            Color.white
-            Color.red
-                
+        ZStack() {
+            LinearGradient(gradient: Gradient(colors: [.red, .blue]), startPoint: .top, endPoint: .bottom)
+                .ignoresSafeArea()
             
-            Text("content")
+            VStack (spacing: 40){
+                VStack {
+                    Text("Tap the country of...")
+                        .foregroundColor(Color.white)
+                        .font(.subheadline.weight(.heavy))
+                    
+                    Text(countries[correctAnswer])
+                        .foregroundColor(Color.white)
+                        .font(.largeTitle.weight(.semibold))
+                }
+                
+                ForEach(0..<3) { number in
+                    Button {
+                        flagTapped(number)
+                    } label: {
+                        Image(countries[number])
+                            .renderingMode(.original)
+                            .clipShape(.buttonBorder)
+                            .shadow(radius: 4)
+                    }
+                }
+                
+            }
+            .foregroundColor(.secondary)
+            .padding(30)
+            .background(.ultraThinMaterial)
+            .cornerRadius(20)
             
         }
-        .ignoresSafeArea()
+        .alert(scoreTitle, isPresented: $showScore) {
+            Button("Play again") {
+                showScore.toggle()
+                askQuestion()
+            }
+        } message: {
+            Text("Your score: \(score)/\(countries.count)")
+        }
+        
+        
+                
     }
+    
+    func flagTapped(_ number: Int) {
+        if number == correctAnswer {
+            scoreTitle = "Correct!"
+            score += 1
+        } else {
+            scoreTitle = "Wrong!"
+        }
+        showScore = true
+    }
+    
+    func askQuestion() {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0..<3)
+    }
+
 }
 
 #Preview {
